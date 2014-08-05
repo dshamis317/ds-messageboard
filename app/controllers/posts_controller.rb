6993337f46c23da4680178e91302a5c2619dfb33
dshamis317 <dshamis317@gmail.com>
2014-08-05 12:46:03 -0400

    displaying posts and permitting title into post_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message, :user_id)
+    params.require(:post).permit(:title, :message, :user_id)
   end
 end
