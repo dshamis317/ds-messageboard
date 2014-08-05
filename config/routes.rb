e2eb3b4141585f7100e7b972c49aa3d65a0d688c
dshamis317 <dshamis317@gmail.com>
2014-08-05 11:32:18 -0400

    built sessions controller and set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # Routes for index page and Posts_Controller
   root 'posts#index'
   resources :posts
+
+  # Routes for logging in
+  get 'sessions/new' => 'sessions#new', as: 'login'
+  post 'sessions' => 'sessions#create'
+  delete 'sessions' => 'sessions#destroy', as: 'logout'
+
 end
